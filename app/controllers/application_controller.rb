9d113a353d5171155d42205ebf8a6489e10cad71
github username <github email address>
2019-04-05 00:48:49 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     user = User.find_by(username: params["username"], password: params["password"])
+    #@current_user = Helpers.current_user
     if user
       session[:user_id] = user.id
       redirect to '/account'
