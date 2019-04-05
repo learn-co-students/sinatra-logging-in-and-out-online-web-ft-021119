7a1d145bac1955c9af7712c702712d1a2fa6b4d6
Lizzie <lchan217@gmail.com>
2019-04-04 20:52:52 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
+require 'pry'
 class Helpers
+  def self.current_user(hash)  #{:user_id=>1}
+    @user = User.find(hash[:user_id])
+    @user
+  end
+  def self.is_logged_in?(hash)#{:user_id=>1}
+    !!hash[:user_id]
+  end
 end
