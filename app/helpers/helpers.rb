e61d8388f48ed703411039254e9076ec84c5e712
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-10 17:05:12 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,8 @@
 class Helpers
-  def self.current_user
-
+  def self.current_user(session)
+    @current_user = User.find_by_id(session[:user_id])
   end
-  def is_logged_in?
-
+  def self.is_logged_in?(session)
+    session[:user_id] != nil ? true : false
   end
 end
